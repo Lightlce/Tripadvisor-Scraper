b049d2a53406b132772aade148030b0d1fcbe14b
Aaron Tan <aarontanws@gmail.com>
2018-02-25 14:31:42 +0800

    Able to scrape one review now

--- Tripadvisor.py
+++ Tripadvisor.py
@@ -3,33 +3,36 @@
 from lxml import html
 from lxml import etree
 import requests
-import json
-import json,re
-from dateutil import parser as dateparser
-from time import sleep
 
 tripadvisor_MAIN_url = 'https://www.tripadvisor.com.sg/'
 
 def tripadvisor_FULL_url(Hotel_ID):
     return tripadvisor_MAIN_url+Hotel_ID
 
+#for i in range(1,1339*5,stop)
+Hotel = tripadvisor_FULL_url('Hotel_Review-g294265-d302294-Reviews-or1-Pan_Pacific_Singapore-Singapore.html')
 
-Hotel = tripadvisor_FULL_url('Hotel_Review-g294265-d302294-Reviews-Pan_Pacific_Singapore-Singapore.html')
-
+#User Agent headers
+#Get more headers: https://udger.com/resources/ua-list/browser-detail?browser=Chrome
 header = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en-US) AppleWebKit/533.4 (KHTML, like Gecko) Chrome/5.0.375.86 Safari/533.4'
 headers = {'User-agent': header}
 
-#https://udger.com/resources/ua-list/browser-detail?browser=Chrome
+#HTML page
 page = requests.get(Hotel,headers = headers,verify=False)
 tree = html.fromstring(page.content)
 
-
 #This will create a list of reviews:
-buyers = tree.xpath('//div[@class="partial_entry"]/text()')
-#This will create a list of prices
-#prices = tree.xpath('//span[@class="item-price"]/text()')
+subject_xpath = tree.xpath('//span[@class="noQuotes"]/text()')
+review_xpath = tree.xpath('//p[@class="partial_entry"]/text()')
+name_xpath = tree.xpath('//div[@class="info_text"]/div/text()')
+date_xpath = tree.xpath('//span[@class="ratingDate"]/text()')
+location_xpath = tree.xpath('//div[@class="userLoc"]/strong/text()')
 
-tree = etree.tostring(tree, pretty_print=True, method="html")
-
-print buyers
-print tree
+print subject_xpath[0]
+print review_xpath[0]
+print name_xpath[0]
+print date_xpath[0]
+print location_xpath[0]
+#for row in subject.findAll('span'):
+#    print row.prettify()
+#print tree
